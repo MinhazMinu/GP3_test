34e0589d217e1cde0fbd5fef044ce438a6a6a8f1
Minhazminu <minhazul.taher@gmail.com>
2023-05-11 13:46:14 +0600

    Header section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Navbar } from "./components";
+import { Header } from "./containers";
 
 import "./App.css";
 
 const App = () => {
-    return <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-      </div>
-    </div>
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+        </div>
+    );
 };
 
 export default App;
